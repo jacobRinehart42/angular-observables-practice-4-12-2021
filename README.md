c156eaec617e3827138a26227e9ed677d80696ac
jacobRinehart42 <jacobofmirkwood@yahoo.com>
2021-04-12 13:46:03 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-observables-practice-4-12-2021
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-observables-practice-4-12-2021)
---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from "@angular/core";
+import { map, filter, take } from "rxjs/operators";
+import { interval, pipe } from "rxjs";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  constructor() {}
+
+  ngOnInit() {
+    interval(1000).subscribe(v => console.log("value is " + v));
+
+
+    // interval(1000)
+    //   .pipe(
+    //     take(3),
+    //     map(v => Date.now())
+    //   )
+    //   .subscribe(value => console.log("Subscriber: " + value));
+  }
+}
